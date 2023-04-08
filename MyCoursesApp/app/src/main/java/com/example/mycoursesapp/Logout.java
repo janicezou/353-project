335855184b7c406beca895a991bc03021078b942
Quinnbx <91432435+Quinnbx@users.noreply.github.com>
2023-04-07 23:42:59 -0400

    Setup(with minor issues to be fixed)

---
+++ MyCoursesApp/app/src/main/java/com/example/mycoursesapp/Logout.java
@@ -0,0 +1,6 @@
+package com.example.mycoursesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Logout extends AppCompatActivity {
+}
---
+++ MyCoursesApp/app/src/main/java/com/example/mycoursesapp/Setup.java
@@ -0,0 +1,88 @@
+package com.example.mycoursesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Setup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onToLogInButtonClick(View v) {
+        Button tv = findViewById(R.id.toLogInField);
+        onConnectButtonClick(tv);
+        // direct to log in page
+    }
+
+    public void onToSignUpButtonClick(View v) {
+        Button tv = findViewById(R.id.toSignUpField);
+        onConnectButtonClick(tv);
+        // direct to sign up page
+    }
+
+    protected String message;
+
+    public void onConnectButtonClick(Button bv) {
+
+        TextView tv = findViewById(R.id.statusField);
+
+        try {
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.execute( () -> {
+                        try {
+                            // assumes that there is a server running on the AVD's host on port 3000
+                            // and that it has a /test endpoint that returns a JSON object with
+                            // a field called "message"
+
+                            URL url = new URL("http://10.0.2.2:3000/test");
+
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                            conn.setRequestMethod("GET");
+                            conn.connect();
+
+                            Scanner in = new Scanner(url.openStream());
+                            String response = in.nextLine();
+
+                            JSONObject jo = new JSONObject(response);
+
+                            // need to set the instance variable in the Activity object
+                            // because we cannot directly access the TextView from here
+                            message = jo.getString("message");
+
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                            message = e.toString();
+                        }
+                    }
+            );
+
+            executor.awaitTermination(2, TimeUnit.SECONDS);
+
+            bv.setBackgroundColor(Color.GREEN);
+            bv.setCompoundDrawablesWithIntrinsicBounds(android.R.drawable.checkbox_on_background, 0, 0, 0);
+            tv.setText(message);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            tv.setText(e.toString());
+        }
+    }
+}
