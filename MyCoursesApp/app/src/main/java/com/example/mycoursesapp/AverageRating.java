934ffb0a0e96175989bf27f765bbb5d368d84871
AryWilson <awilson2@brynmawr.edu>
2023-04-12 19:52:12 -0400

    fix rating display

--- MyCoursesApp/app/src/main/java/com/example/mycoursesapp/AverageRating.java
+++ MyCoursesApp/app/src/main/java/com/example/mycoursesapp/AverageRating.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,31 +29,48 @@ public class AverageRating {
                         System.out.println("Unexpected status code: " + responseCode);
                     } else {
                         in = new Scanner(url.openStream());
+                        Pattern regex = Pattern.compile("Average Rating: ([0-5].?[0-5]*)");
                         while (in.hasNext()) {
                             String line = in.nextLine();
-                            String pattern = "Average Rating: ";
-                            Pattern p = Pattern.compile(pattern);
-                            Matcher m = p.matcher(line);
-                            Integer index = null;
-                            while (m.find()) {
-                                index = m.start() + pattern.length();
-                            }
-                            if (index != null) {
-                                String r = line.substring(index);
-                                String result = r.substring(0, 1);
+//                            String pattern = "Average Rating: ";
+//                            Pattern p = Pattern.compile(pattern);
+//                            Matcher m = p.matcher(line);
+                            Matcher matcher = regex.matcher(line);
+
+                            if(matcher.find()){
                                 try {
-                                    averageRating = Integer.parseInt(result)*1.0;
+                                    averageRating = Double.parseDouble(matcher.group(1))*1.0;
+                                    System.out.println(averageRating);
                                 } catch (NumberFormatException nfe) {
                                     nfe.printStackTrace();
                                 }
                             }
+//                            Integer index = null;
+//                            while (m.find()) {
+//                                index = m.start() + pattern.length();
+//                            }
+//                            if (index != null) {
+//                                String r = line.substring(index);
+//                                String result = r.substring(0, 1);
+//                                try {
+//                                    averageRating = Integer.parseInt(result)*1.0;
+//                                } catch (NumberFormatException nfe) {
+//                                    nfe.printStackTrace();
+//                                }
+//                            }
                         }
+                    }
+
+                    if (conn != null) {
+                        conn.disconnect();
                     }
 
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             });
+            // wait for thread to return with updated average value
+            executor.awaitTermination(2, TimeUnit.SECONDS);
         } catch(Exception ee) {
             ee.printStackTrace();
         }
